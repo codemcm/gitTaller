c909d7152ee71e575b323cc582c93dbb540059cf
Martin Cruz M <codemcm@hotmail.com>
2021-01-18 08:43:53 -0600

    changes on files

---
+++ RestApi.js
@@ -0,0 +1,24 @@
+/**
+ * Instalar express generator
+
+npm install express-generator -g
+
+Crear app 
+express --view=pug eventrest
+
+en el directorio ejecutar
+
+npm install
+
+Levantar el servicio
+npm start 
+ *
+
+ PARA POSTGRESQL
+
+ npm install pg-promise
+
+ Instalar blue bird
+
+ npm install bluebird
+ */
